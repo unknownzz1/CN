b31961bf6108d1790f4bf7e71ca1af8fcc65d0cf
unknownzz1 <unknownzz12234@gmail.com>
2026-05-07 00:15:41 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,22 @@
+import socket
 
+server = socket.socket()
+server.bind(("localhost", 12345))
+server.listen(1)
+
+print("Server waiting...")
+
+conn, addr = server.accept()
+print("Connected to", addr)
+
+while True:
+    data = conn.recv(1024).decode()
+    print("Client:", data)
+
+    msg = input("You: ")
+    conn.send(msg.encode())
+
+    if msg.lower() == "exit":
+        break
+
+conn.close()
